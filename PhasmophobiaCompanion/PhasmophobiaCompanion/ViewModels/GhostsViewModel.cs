20779f0df054b06fc086855b5e897847eaec4187
sanyo <sanyoko08@gmail.com>
2023-09-12 18:49:15 +0300

    Добавил ViewModel и View для призраков и иконок

---
+++ PhasmophobiaCompanion/PhasmophobiaCompanion/ViewModels/GhostsViewModel.cs
@@ -0,0 +1,28 @@
+﻿using PhasmophobiaCompanion.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PhasmophobiaCompanion.ViewModels
+{
+    public class GhostsViewModel : BaseViewModel, ISearchable, IFilterable
+    {
+        public List<IListItem> ghosts;
+        public List<IListItem> Ghosts
+        {
+            get { return ghosts; }
+            set { ghosts = value;
+                OnPropertyChanged();
+            }
+        }
+        public List<IListItem> Filter(string filterCriteria)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Search(string query)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
