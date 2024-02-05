73cafe02ee8da4107c044b57074387f1e686e6ac
sanyo <sanyoko08@gmail.com>
2024-02-05 15:32:44 +0300

    Добавлены обработчики события для списков скоростей призраков и рассудка для начала охоты - GhostSpeed и SanityHunt соответсвенно. Добавлена страница всплывающей подсказки.

--- PhasmophobiaCompanion/PhasmophobiaCompanion/Views/MainPage.xaml.cs
+++ PhasmophobiaCompanion/PhasmophobiaCompanion/Views/MainPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using PhasmophobiaCompanion.Models;
 using PhasmophobiaCompanion.ViewModels;
+using Rg.Plugins.Popup.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -40,6 +41,68 @@ namespace PhasmophobiaCompanion.Views
                     await Launcher.OpenAsync(uri);
                 }
             }
+        }
+
+        private async void OnMinGhostSpeedTapped(object sender, EventArgs e)
+        {
+            var parentLabel = sender as Label;
+            if (parentLabel?.BindingContext is Ghost ghostItem)
+            {
+                var minGhostSpeedClause = ghostItem.MinGhostSpeedClause;
+                await PopupNavigation.Instance.PushAsync(new TooltipPopup(minGhostSpeedClause));
+            }
+        }
+
+        private void OnGhostTapped(object sender, EventArgs e)
+        {
+            var parentLabel = sender as Label;
+            if (parentLabel?.BindingContext is Ghost ghostItem)
+            {
+                var Page = new GhostDetailPage(ghostItem);
+                Application.Current.MainPage.Navigation.PushAsync(Page);
+            }
+        }
+
+        private async void OnMaxGhostSpeedTapped(object sender, EventArgs e)
+        {
+            var parentLabel = sender as Label;
+            if (parentLabel?.BindingContext is Ghost ghostItem)
+            {
+                var maxGhostSpeedClause = ghostItem.MaxGhostSpeedClause;
+                await PopupNavigation.Instance.PushAsync(new TooltipPopup(maxGhostSpeedClause));
+            }
+        }
+
+        private async void OnMaxGhostSpeedLoSTapped(object sender, EventArgs e)
+        {
+            var parentLabel = sender as Label;
+            if (parentLabel?.BindingContext is Ghost ghostItem)
+            {
+                var maxGhostSpeedLoSClause = ghostItem.MaxGhostSpeedLoSClause;
+                await PopupNavigation.Instance.PushAsync(new TooltipPopup(maxGhostSpeedLoSClause));
+            }
+        }
+
+        private async void OnMinSanityHuntTapped(object sender, EventArgs e)
+        {
+            var parentLabel = sender as Label;
+            if (parentLabel?.BindingContext is Ghost ghostItem)
+            {
+                var minSanityHuntClause = ghostItem.MinSanityHuntClause;
+                await PopupNavigation.Instance.PushAsync(new TooltipPopup(minSanityHuntClause));
+            }
+
+        }
+
+        private async void OnMaxSanityHuntTapped(object sender, EventArgs e)
+        {
+            var parentLabel = sender as Label;
+            if (parentLabel?.BindingContext is Ghost ghostItem)
+            {
+                var maxSanityHuntClause = ghostItem.MaxSanityHuntClause;
+                await PopupNavigation.Instance.PushAsync(new TooltipPopup(maxSanityHuntClause));
+            }
+
         }
     }
 }
---
+++ PhasmophobiaCompanion/PhasmophobiaCompanion/Views/TooltipPopup.xaml.cs
@@ -0,0 +1,30 @@
+﻿using Rg.Plugins.Popup.Pages;
+using Rg.Plugins.Popup.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PhasmophobiaCompanion.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class TooltipPopup : PopupPage
+    {
+        public string Message { get; set; }
+        public TooltipPopup(string message)
+        {
+            InitializeComponent();
+            Message = message;
+            BindingContext = this;
+        }
+        protected override bool OnBackgroundClicked()
+        {
+            PopupNavigation.Instance.PopAsync();
+            return false;
+        }
+    }
+}
