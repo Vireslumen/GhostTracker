217d82481998350df375f51f5e57a02c64e7a95c
sanyo <sanyoko08@gmail.com>
2024-02-22 15:27:46 +0300

    Добавлен обработчик нажатия на улики на подробной странице призрака. При нажатии выполняется переход на подробную страницу улики.

--- PhasmophobiaCompanion/PhasmophobiaCompanion/ViewModels/GhostDetailViewModel.cs
+++ PhasmophobiaCompanion/PhasmophobiaCompanion/ViewModels/GhostDetailViewModel.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.Windows.Input;
 using PhasmophobiaCompanion.Models;
+using PhasmophobiaCompanion.Views;
 using Serilog;
+using Xamarin.Forms;
 
 namespace PhasmophobiaCompanion.ViewModels
 {
@@ -16,6 +19,7 @@ namespace PhasmophobiaCompanion.ViewModels
             try
             {
                 Ghost = ghost;
+                ClueSelectedCommand = new Command<Clue>(OnClueSelected);
             }
             catch (Exception ex)
             {
@@ -31,6 +35,28 @@ namespace PhasmophobiaCompanion.ViewModels
             {
                 ghost = value;
                 OnPropertyChanged();
+            }
+        }
+        public ICommand ClueSelectedCommand { get; private set; }
+
+        /// <summary>
+        ///     Переход на подробную страницу улики.
+        /// </summary>
+        /// <param name="clue">Выбранная улика</param>
+        private async void OnClueSelected(Clue clue)
+        {
+            try
+            {
+                if (clue == null) return;
+                // Логика для открытия страницы деталей призрака
+                var detailPage = new ClueDetailPage(clue);
+                await Application.Current.MainPage.Navigation.PushAsync(detailPage);
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex,
+                    "Ошибка во время перехода на подробную страницу улики из подробной страницы призраков GhostDetailPage.");
+                throw;
             }
         }
     }
