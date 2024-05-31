5ca08125cfb5eca5e6d61bf30ca6ce945b21004e
Александр <sanyoko08@gmail.com>
2024-05-31 15:17:39 +0300

    База данных частично заполнена. Исправлены некоторые изображения и добавлены новый изображения. Полностью переработано отображение раскрывающихся элементов на подробных страницах: тело раскрывающегося элемента является webview, максимум 3 раскрытых элемента, добавлен кастомный рендер для webview, логика раскрытия перенесена в новый базовый класс UnfoldingItemsViewModel.

--- PhasmophobiaCompanion/PhasmophobiaCompanion/ViewModels/ClueDetailViewModel.cs
+++ PhasmophobiaCompanion/PhasmophobiaCompanion/ViewModels/ClueDetailViewModel.cs
@@ -14,7 +14,7 @@ namespace PhasmophobiaCompanion.ViewModels
     /// <summary>
     ///     ViewModel для подробной страницы улики.
     /// </summary>
-    public class ClueDetailViewModel : BaseViewModel
+    public class ClueDetailViewModel : UnfoldingItemsViewModel
     {
         public readonly DataService dataService;
         public ICommand ImageTappedCommand;
